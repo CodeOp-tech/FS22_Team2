7e643e18ec774d47b9a705093a9cb223e8284a12
Zoe Laventhol <zoe.laventhol@gmail.com>
2022-11-12 14:58:09 +0100

    initialized users table with 2 test users

--- model/init_db.sql
+++ model/init_db.sql
@@ -1,22 +1,16 @@
-DROP TABLE IF EXISTS skateMoves;
-
-
-CREATE TABLE skateMoves(
-    id INT NOT NULL PRIMARY KEY,
-    level VARCHAR(255),
-    name VARCHAR(255),
-    video VARCHAR(255),
-    category VARCHAR(255),
-    description VARCHAR(500)
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    user-id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    shop-id INT,
+    username VARCHAR(255),
+    user-email VARCHAR(255),
+    user-points INT DEFAULT 0,
 );
 
-INSERT INTO skateMoves (id, level, name, video, category, description)
-VALUES (1, "Beginner", "PloughStop","vTransition.mp4", "stops", "Plow/plough stop is a must-have skill for any roller skater. It is one of the basics method of stopping, the first one to learn before t-stop and turn-around toe stop."),
-(2, "Intermediate", "TStop","tStop.mp4", "stops", "The T-stop is a very easy and effective way to come to a full halt."),
-(3, "Advanced", "ToeStop", "toeStop.mp4", "stops", "The toe stops come in handy for much more than breaking. By mastering these 5 techniques your life as a roller skater will become much easier and fun.Using the toe stop as a break takes a little to get used to. But by using the instructions in this video, you’ll get the hold of it in no time."),
-(4, "Beginner", "VTransition", "vTransition.mp4", "transitions", "this move does not take that long to learn and master.If you are having trouble maintaining balance with this transition, hold your arms out to the side. This will prevent you from falling over to one side and maintain a good, set balance."),
-(5, "Intermediate", "PivotTransition", "pivotTransition.mp4", "transitions", "Now this one can be a little bit scary if you're going at speed to transition. Take it slow and steady!"),
-(6, "Advanced", "ManualTransition", "manuelTransition.mp4", "transitions", "staggering the feet is the most important step,The trick is to look forward while coming into the jump, and keep looking the same way after you jump, No need to turn your head.");
+INSERT INTO users (user-id, shop-id, username, user-email, user-points)
+VALUES 
+ (1, null, buyer1, buyer1@msb.inc, 0)
+ (2, 1, seller1, seller1@msb.inc, 0);
 
 -- CREATE TABLE cats(
 --     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
