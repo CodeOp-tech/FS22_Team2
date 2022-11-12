bc86d8806b593496a8061088b2c31eb52998c62c
Zoe Laventhol <zoe.laventhol@gmail.com>
2022-11-12 14:58:58 +0100

    cleaned up comments on initDb.sql

--- model/init_db.sql
+++ model/init_db.sql
@@ -11,17 +11,3 @@ INSERT INTO users (user-id, shop-id, username, user-email, user-points)
 VALUES 
  (1, null, buyer1, buyer1@msb.inc, 0)
  (2, 1, seller1, seller1@msb.inc, 0);
-
--- CREATE TABLE cats(
---     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
---     name VARCHAR(100),
---     video VARCHAR(100),
---     category VARCHAR(100)
--- );
--- INSERT INTO cats (id,  name, video, category)
--- VALUES (1, "beginner", "ploughStop","vTransition.mp4", "stops"),
--- (2, "intermediate", "tStop","tStop.mp4", "stops"),
--- (3, "advanced", "toeStop", "toeStop.mp4", "stops"),
--- (4, "beginner", "vTransition", "vTransition.mp4", "transitions"),
--- (5, "intermediate", "pivotTransition", "pivotTransition.mp4", "transitions"),
--- (6, "advanced", "manuelTransition", "manuelTransition.mp4", "transitions");
