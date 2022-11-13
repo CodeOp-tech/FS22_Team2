080aae3e4316fdee111bc737d266043d522c3f46
Zoe Laventhol <zoe.laventhol@gmail.com>
2022-11-13 13:43:29 +0100

    ensureSameUser route  in users.js, create user profile page

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,18 @@ var router = express.Router();
 
 // GET one user
 // Ensure user can only see their own profile
-router.get('/user-id', ensureSameUser, async function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/userId', ensureSameUser, async function(req, res, next) {
+  let { userId } = req.params;
+  let sql = `SELECT * FROM users WHERE user-id == ${userId};`
+
+  try {
+    let results = await db(sql);
+    let user = results.data[0];
+    delete user.password;
+    res.send(user);
+  } catch (err) {
+    res.status(500).send({ error: err.message })
+  }
 });
 
 module.exports = router;
