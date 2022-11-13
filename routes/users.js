857c4bfc8281b4abb75e5668e7ea75fd2f9f5c89
Zoe Laventhol <zoe.laventhol@gmail.com>
2022-11-13 15:23:42 +0100

    added get all users route to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const { ensureSameUser } = require('../middleware/guards');
+const db = require('../model/helper.js')
+
+// GET all users
+router.get('/', async function(req, res, next) {
+  let sql = 'SELECT * FORM users ORDER BY username';
+
+  try {
+    let results = await db(sql);
+    let users = results.data;
+    users.forEach(u => delete u.password);
+    res.send(users);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+}
+
+)
+
 
 // GET one user
 // Ensure user can only see their own profile
