6ab97c3b9591fd9b665dd70599d09c7fab3096d6
jeszeo24 <77438726+jeszeo24@users.noreply.github.com>
2022-11-14 15:29:59 -0500

    products init_db.sql updated with stripe_product_id

--- model/init_db.sql
+++ model/init_db.sql
@@ -48,9 +48,9 @@ CREATE TABLE `products` (
 
 INSERT INTO products (product_name, price, product_image, product_quantity, product_description, shop_id)
 VALUES 
-('Coffee', 5.99, 'https://www.tastingtable.com/img/gallery/coffee-brands-ranked-from-worst-to-best/l-intro-1645231221.jpg', 20, 'Coffee brewed and served fresh in-store', 1),
-('Sunglasses', 10.99, 'https://images.unsplash.com/photo-1577803645773-f96470509666?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c3VuZ2xhc3Nlc3xlbnwwfHwwfHw%3D&w=1000&q=80', 10, 'Fancy sunglasses', 2),
-('Camera', 59.99, 'https://t3.ftcdn.net/jpg/00/79/36/04/360_F_79360425_13tH0FGR7nYTNlXWKOWtLmzk7BAikO1b.jpg', 50, 'The best camera around', 2);
+('Coffee', 5.99, 'https://www.tastingtable.com/img/gallery/coffee-brands-ranked-from-worst-to-best/l-intro-1645231221.jpg', 20, 'Coffee brewed and served fresh in-store', 'price_1M44bEBIwndE5957lW1avj2o', 1),
+('Sunglasses', 10.99, 'https://images.unsplash.com/photo-1577803645773-f96470509666?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c3VuZ2xhc3Nlc3xlbnwwfHwwfHw%3D&w=1000&q=80', 10, 'Defend yourself from all that shade you be getting wearing these babies', 'price_1M49GDBIwndE5957XZPxGANH', 2),
+('Camera', 59.99, 'https://t3.ftcdn.net/jpg/00/79/36/04/360_F_79360425_13tH0FGR7nYTNlXWKOWtLmzk7BAikO1b.jpg', 50, 'The best camera around', 'price_1M49HEBIwndE59571OqKQqvk', 2);
 
 CREATE TABLE `purchases` (
 	`purchase_id` INT NOT NULL AUTO_INCREMENT,
