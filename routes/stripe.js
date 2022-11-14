9fc5657369cfe5a6ec9fb09ad0fa3510f4e3b9dc
jeszeo24 <77438726+jeszeo24@users.noreply.github.com>
2022-11-14 15:37:40 -0500

    Update of products init_db.sql insert

--- routes/stripe.js
+++ routes/stripe.js
@@ -21,6 +21,8 @@ router.post('/checkout', async function(req, res,) {
     ]
     */
 
+    console.log(req.body);
+    
     const items = req.body.items;
     let lineItems = []; // what Stripe calls items for API call
     items.forEach((item) => {
@@ -36,13 +38,13 @@ router.post('/checkout', async function(req, res,) {
     const session = await stripe.checkout.sessions.create({ // create properties below
         line_items: lineItems, 
         mode: "payment", 
-        success_url: "http://localhost:3000/success",
-        cancel_url: "http://localhost:3000/cancel"
+        success_url: "http://localhost:3000/success", // if checkout is successful
+        cancel_url: "http://localhost:3000/cancel" // if checkout fails
     })
 
     // now have access to URL for user to checkout
-    res.send(JSON.stringify({
-        url: session.url // allows us to show user session created for them
+    res.send(JSON.stringify({ // send object to front end, with one property
+        url: session.url // allows us to show user session that Stripe created for them
     }));
 
   });
