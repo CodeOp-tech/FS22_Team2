f4b118236412d81c4d7e65bdb2e150f35b801954
Megan Walsh <megan.katie.walsh@gmail.com>
2022-11-23 16:58:51 +0100

    working maps

--- routes/shops.js
+++ routes/shops.js
@@ -3,12 +3,53 @@ var router = express.Router();
 const { ensureSameUser, ensureShopOwner } = require("../middleware/guards");
 const db = require("../model/helper.js");
 
-// GET all shops
-// doesn't need protection
 router.get("/", async function (req, res, next) {
-  let sql = `SELECT DISTINCT shops.* WHERE products.product_name IN ( "scarf", "sponge")`;
+  let sql = `SELECT * FROM shops`;
+  try {
+    let results = await db(sql);
+    let shops = results.data;
+    res.send(shops);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+//helper
+function makeWhereFromFilters(query) {
+  let filters = [];
+  let words = query.product_name.split(",").map((w) => `"${w}"`);
+  filters.push(words);
+  return " IN (" + filters.join(", ") + ")";
+}
+
+router.get("/search/products", async function (req, res, next) {
+  let sql = `SELECT DISTINCT shops.*, products.*
+  FROM shops LEFT JOIN products ON shops.shop_id = products.shop_id`;
+  let where = makeWhereFromFilters(req.query);
+  console.log(req.query, "***********************");
+  // make optional WHERE-part from query parameters
+  // If query parameters were passed, append them to the SELECT statement
+  if (where) {
+    sql += ` WHERE products.product_name ${where}`;
+  }
+  console.log(sql);
+  // GET all shops (with filter)
+  // router.get("/cats", async (req, res) => {
+  //   let sql = "SELECT * FROM shops";
+  //   let where = makeWhereFromFilters(req.query); // make optional WHERE-part from query parameters
+  //   // If query parameters were passed, append them to the SELECT statement
+  //   if (where) {
+  //     sql += ` WHERE ${productString}`;
+  //   }
+  // });
+
+  //let sql = `SELECT DISTINCT shops.* WHERE products.product_name IN ('coffee', 'scarf', 'belt')`;
+
+  //`SELECT DISTINCT shops.* WHERE products.product_name IN ( "scarf", "sponge")`;
+
   // FROM shops
   //      LEFT JOIN products ON shops.shop_id = products.shop_id `;
+
   //WHERE products.product_name IN ( "scarf", "sponge")
 
   //  fetch from client (this is the filter) Get /shops?product=scarf,sponge
@@ -93,73 +134,111 @@ router.get("/:userId", ensureSameUser, async function (req, res, next) {
 
 // PUT edit shop info
 // PROTECTED - user should only be able to enter own shop
-router.put("/edit/:shop_id", ensureShopOwner, async (req, res) => { 
-    let { shop_id }  = req.params;
-    let { shopData: {shop_name, shop_description, shop_image, shop_address,  website, phone, shop_email, donate, led_lights, small_biz, min_biz, wo_biz} } = req.body;
-    // let sql = `SELECT * FROM shops WHERE shop_id = ${Number(shop_id)};`;
+router.put("/edit/:shop_id", ensureShopOwner, async (req, res) => {
+  let { shop_id } = req.params;
+  let {
+    shopData: {
+      shop_name,
+      shop_description,
+      shop_image,
+      shop_address,
+      website,
+      phone,
+      shop_email,
+      donate,
+      led_lights,
+      small_biz,
+      min_biz,
+      wo_biz,
+    },
+  } = req.body;
+  // let sql = `SELECT * FROM shops WHERE shop_id = ${Number(shop_id)};`;
 
-    try {
-        // let shopResult= await db(sql);
-        // let shop = shopResult.data[0];
-      if (shop_name) {
-        await db(
-          `UPDATE shops SET shop_name='${shop_name}' WHERE shop_id=${shop_id}`
-        );
-      }
-  
-      if (shop_address) {
-        await db(`UPDATE shops SET shop_address='${shop_address}' WHERE shop_id=${shop_id}`);
-      }
-  
-      if (shop_description) {
-        await db(
-          `UPDATE shops SET shop_description='${shop_description}' WHERE shop_id=${shop_id}`
-        );
-      }
-  
-      if (shop_image) {
-        await db(`UPDATE shops SET shop_image='${shop_image}' WHERE shop_id=${shop_id}`);
-      }
-
-      if (website) {
-        await db(`UPDATE shops SET website='${website}' WHERE shop_id=${shop_id}`);
-      }
-  
-      if (phone) {
-        await db(`UPDATE shops SET phone='${phone}' WHERE shop_id=${shop_id}`);
-      }
-
-      if (shop_email) {
-        await db(`UPDATE shops SET shop_email='${shop_email}' WHERE shop_id=${shop_id}`);
-      }
-
-      if (donate) {
-        await db(`UPDATE shops SET donate='${Number(donate)}' WHERE shop_id=${shop_id}`);
-      }
-
-      if (led_lights) {
-        await db(`UPDATE shops SET led_lights='${Number(led_lights)}' WHERE shop_id=${shop_id}`);
-      }
-
-      if (small_biz) {
-        await db(`UPDATE shops SET small_biz='${Number(small_biz)}' WHERE shop_id=${shop_id}`);
-      }
-
-      if (min_biz) {
-        await db(`UPDATE shops SET min_biz='${Number(min_biz)}' WHERE shop_id=${shop_id}`);
-      }
-
-      if (wo_biz) {
-        await db(`UPDATE shops SET wo_biz='${Number(wo_biz)}' WHERE shop_id=${shop_id}`);
-      }
-
-      const results = await db(`SELECT * FROM shops WHERE shop_id = ${Number(shop_id)}`);
-  
-      res.status(201).send(results.data); 
-    } catch (err) {
-      res.status(500).send({ error: err.message });
+  try {
+    // let shopResult= await db(sql);
+    // let shop = shopResult.data[0];
+    if (shop_name) {
+      await db(
+        `UPDATE shops SET shop_name='${shop_name}' WHERE shop_id=${shop_id}`
+      );
     }
+
+    if (shop_address) {
+      await db(
+        `UPDATE shops SET shop_address='${shop_address}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (shop_description) {
+      await db(
+        `UPDATE shops SET shop_description='${shop_description}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (shop_image) {
+      await db(
+        `UPDATE shops SET shop_image='${shop_image}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (website) {
+      await db(
+        `UPDATE shops SET website='${website}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (phone) {
+      await db(`UPDATE shops SET phone='${phone}' WHERE shop_id=${shop_id}`);
+    }
+
+    if (shop_email) {
+      await db(
+        `UPDATE shops SET shop_email='${shop_email}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (donate) {
+      await db(
+        `UPDATE shops SET donate='${Number(donate)}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (led_lights) {
+      await db(
+        `UPDATE shops SET led_lights='${Number(
+          led_lights
+        )}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (small_biz) {
+      await db(
+        `UPDATE shops SET small_biz='${Number(
+          small_biz
+        )}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (min_biz) {
+      await db(
+        `UPDATE shops SET min_biz='${Number(min_biz)}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    if (wo_biz) {
+      await db(
+        `UPDATE shops SET wo_biz='${Number(wo_biz)}' WHERE shop_id=${shop_id}`
+      );
+    }
+
+    const results = await db(
+      `SELECT * FROM shops WHERE shop_id = ${Number(shop_id)}`
+    );
+
+    res.status(201).send(results.data);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 });
 
 module.exports = router;
-
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { ensureSameUser } = require('../middleware/guards');
-const db = require('../model/helper.js')
+const { ensureSameUser } = require("../middleware/guards");
+const db = require("../model/helper.js");
 
 // GET all users
 // NOT PROTECTED: get username and points for leaderboard, remove al other info. If we add other info to db, remember to delete later
-router.get('/', async function(req, res, next) {
-  let sql = 'SELECT * FROM users ORDER BY username';
+router.get("/", async function (req, res, next) {
+  let sql = "SELECT * FROM users ORDER BY username";
 
   try {
     let results = await db(sql);
     let users = results.data;
-    users.forEach(u => delete u.password);
-    users.forEach(u => delete u.email);
-    users.forEach(u => delete u.shop_id);
+    users.forEach((u) => delete u.password);
+    users.forEach((u) => delete u.email);
+    users.forEach((u) => delete u.shop_id);
     res.send(users);
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
 });
 
-
 // GET one user
 // PROTECTED: user can only see their own profile
-router.get('/:userId', ensureSameUser, async function(req, res, next) {
+router.get("/:userId", async function (req, res, next) {
   let { userId } = req.params;
-  let sql = `SELECT * FROM users WHERE user_id = ${Number(userId)};`
+  let sql = `SELECT * FROM users WHERE user_id = ${Number(userId)};`;
 
   try {
     let results = await db(sql);
@@ -33,16 +32,16 @@ router.get('/:userId', ensureSameUser, async function(req, res, next) {
     delete user.password;
     res.send(user);
   } catch (err) {
-    res.status(500).send({ error: err.message })
+    res.status(500).send({ error: err.message });
   }
 });
 
 // PUT add user points from purchase
 // PROTECT: ensureSameUser
 // is it appropriate to use INNER JOIN here instead of LEFT JOIN? B/c we want to only return users who have purchases
-router.get('/points/:user_id', async function(req, res, next) {
+router.get("/points/:user_id", async function (req, res, next) {
   let { user_id } = req.params;
-  // access the purchase points via user > purchases > purchased_items 
+  // access the purchase points via user > purchases > purchased_items
   // original sqlGet select: SELECT users.*, purchases.user_id, purchases.purchase_id, purchased_items.purchase_id, purchased_items.purchase_points
   let sqlGet = `
     SELECT users.*, purchases.user_id, purchases.purchase_id, purchased_items.purchase_id, purchased_items.purchase_points
@@ -50,14 +49,14 @@ router.get('/points/:user_id', async function(req, res, next) {
       LEFT JOIN purchases ON users.user_id = purchases.user_id 
       LEFT JOIN purchased_items ON purchases.purchase_id = purchased_items.purchase_id
       WHERE users.user_id = ${Number(user_id)}
-  `
+  `;
   // sum all purchase points
   let sqlSum = `SELECT SUM(total) WHERE users.user_id = ${Number(user_id)}
-  `
+  `;
   // add summed purchase_points to user_points
   let sqlPut = `UPDATE users SET users.user_points={}
   WHERE users.user_id = ${Number(user_id)}
-  `
+  `;
   // EXAMPLE PUT
   // if (shop_name) {
   //   await db(
@@ -66,19 +65,17 @@ router.get('/points/:user_id', async function(req, res, next) {
   // }
 
   try {
-    let getResults = await db(sqlGet); 
+    let getResults = await db(sqlGet);
     let getData = getResults.data[0];
     delete getData.password;
-    
+
     let sumResults = await db(`SELECT SUM(${getData.purchase_points})`);
     let sumData = sumResults.data[0];
-    
-    res.send(getData); 
-} catch(err) { 
-    res.status(500).send({ error: err.message })
-}
+
+    res.send(getData);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 });
-
-
 
 module.exports = router;
