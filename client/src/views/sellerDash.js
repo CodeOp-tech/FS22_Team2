a430c8891accf7fb1a3091bd1d1e509785ec1326
Megan Walsh <megan.katie.walsh@gmail.com>
2022-11-24 10:33:34 +0100

    okk

--- client/src/views/sellerDash.js
+++ client/src/views/sellerDash.js
@@ -6,11 +6,10 @@ import { Container, Col, Row, Card, Button } from "react-bootstrap";
 import ShopEditForm from "../components/ShopEditForm";
 import Api from "../helpers/Api.js";
 import Local from "../helpers/Local";
-import Accordion from 'react-bootstrap/Accordion'
+import Accordion from "react-bootstrap/Accordion";
 
 function SellerDash(props) {
   const [productsData, setProductsData] = useState([]);
-
 
   useEffect(() => {
     getProducts();
@@ -35,18 +34,12 @@ function SellerDash(props) {
     // console.log(formData);
     let options = {
       method: "POST",
-<<<<<<< HEAD
-      //headers: { 'Content-Type': 'application/json' }, //remove?
-      body: formData
-||||||| merged common ancestors
-      //headers: { 'Content-Type': 'application/json' }, //remove?
-      body:  formData
-=======
-      body:  formData
->>>>>>> main
+      body: formData,
     };
-<<<<<<< HEAD
-
+    let token = Local.getToken();
+    if (token) {
+      options.headers["authorization"] = `Bearer ${token}`;
+    } // add token to headers if it exists in localStorage
     try {
       let response = await fetch(`/products/${Local.getShopId()}`, options);
       if (response.ok) {
@@ -58,233 +51,90 @@ function SellerDash(props) {
       } else {
         console.log(`Server error: ${response.status} ${response.statusText}`);
       }
-      // add token to headers if it exists in localStorage
-      let token = Local.getToken();
-      if (token) {
-        options.headers['authorization'] = `Bearer ${token}`;
-      }
-    
-      try {
-        let response = await fetch(`/products/${Local.getShopId()}`, options);
-        if (response.ok) {
-          let result = await response.json();
-          setProductsData(result);
-          getProducts(); //note to ask about the reload
-        } else {
-          console.log(`Server error: ${response.status} ${response.statusText}`);
-        }
-      } catch (err) {
-||||||| merged common ancestors
+    } catch (err) {
+      console.log(`Network error: ${err.message}`);
+    }
+  }
 
-  try {
-    let response = await fetch(`/products/${Local.getShopId()}`, options); 
-    if (response.ok) {
-    let result = await response.json();
-    setProductsData(result);
-    getProducts();
-    props.showAllProducts(); // fetches all the products without reload on the online store
-     //note to ask about the reload
-    } else {
-      console.log(`Server error: ${response.status} ${response.statusText}`);
-    }
-    // add token to headers if it exists in localStorage
-    let token = Local.getToken();
-    if(token) {
-        options.headers['authorization'] = `Bearer ${token}`;
-    }
-    
+  async function deleteProduct(shop_id, product_id) {
+    let options = {
+      method: "DELETE",
+    };
     try {
-      let response = await fetch(`/products/${Local.getShopId()}`, options); 
+      let response = await fetch(`/products/${shop_id}/${product_id}`, options);
+
       if (response.ok) {
-      let result = await response.json();
-      setProductsData(result);
-      getProducts(); //note to ask about the reload
+        let result = await response.json();
+        setProductsData(result);
+        getProducts();
+        props.showAllProducts(); //split second loads all products
       } else {
         console.log(`Server error: ${response.status} ${response.statusText}`);
       }
     } catch (err) {
-=======
-    let token = Local.getToken();
-    if(token) {
-        options.headers['authorization'] = `Bearer ${token}`;
-    } // add token to headers if it exists in localStorage
-  try {
-    let response = await fetch(`/products/${Local.getShopId()}`, options); 
-      if (response.ok) {
-    let result = await response.json();
-      setProductsData(result);
-      getProducts();
-      props.showAllProducts(); // fetches all the products without reload on the online store
-     //note to ask about the reload
-    } else {
-      console.log(`Server error: ${response.status} ${response.statusText}`);
-    }
-    } catch (err) {
->>>>>>> main
-        console.log(`Network error: ${err.message}`);
-<<<<<<< HEAD
-      }
-    }
-||||||| merged common ancestors
-      }
-  }
-=======
-    } 
-}
->>>>>>> main
-
-  async function deleteProduct(shop_id, product_id) {
-      let options = {
-        method: "DELETE",
-      };
-      try {
-        let response = await fetch(`/products/${shop_id}/${product_id}`, options);
-
-<<<<<<< HEAD
-        if (response.ok) {
-          let result = await response.json();
-          setProductsData(result);
-          getProducts()
-          props.showAllProducts(); //split second loads all products
-        } else {
-          console.log(`Server error: ${response.status} ${response.statusText}`);
-        }
-      }
-||||||| merged common ancestors
-    if (response.ok) {
-      let result = await response.json();
-      setProductsData(result);
-      getProducts()
-      props.showAllProducts(); //split second loads all products
-    } else {
-      console.log(`Server error: ${response.status} ${response.statusText}`);
-    }
-  }
-=======
-    if (response.ok) {
-      let result = await response.json();
-      setProductsData(result);
-      getProducts()
-      props.showAllProducts(); //split second loads all products
-    } else {
-      console.log(`Server error: ${response.status} ${response.statusText}`);
-    }
-    } catch (err) {
       console.log(`Server error: ${err.message}`);
     }
   }
->>>>>>> main
 
-    async function editProduct(shop_id, product_id, formData) {
-<<<<<<< HEAD
-        let options = {
-          method: 'PUT',
-          // headers: { 'Content-Type': 'application/json' },
-          body: formData,
-        };
-||||||| merged common ancestors
-      let options = {
-      method: 'PUT',
+  async function editProduct(shop_id, product_id, formData) {
+    let options = {
+      method: "PUT",
       // headers: { 'Content-Type': 'application/json' },
       body: formData,
-  };
-=======
-      let options = {
-      method: 'PUT',
-      // headers: { 'Content-Type': 'application/json' },
-      body: formData
-  };
->>>>>>> main
+    };
 
-<<<<<<< HEAD
-        try {
-          let response = await fetch(`/products/${shop_id}/${product_id}`, options);
-          if (response.ok) {
-            let result = await response.json();
-            setProductsData(result);
-          } else {
-            console.log(`Server error: ${response.status} ${response.statusText}`);
-          }
-        } catch (err) {
-          console.log(`Server error: ${err.message}`);
-        }
-||||||| merged common ancestors
-  try {
+    try {
       let response = await fetch(`/products/${shop_id}/${product_id}`, options);
       if (response.ok) {
         let result = await response.json();
         setProductsData(result);
       } else {
         console.log(`Server error: ${response.status} ${response.statusText}`);
-=======
-  try {
-      let response = await fetch(`/products/${shop_id}/${product_id}`, options);
-        if (response.ok) {
-          let result = await response.json();
-          setProductsData(result);
-      } else {
-        console.log(`Server error: ${response.status} ${response.statusText}`);
->>>>>>> main
       }
-
-<<<<<<< HEAD
-      return (
-        <Accordion defaultActiveKey="0" >
-          <Container>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header className='editShop-accHead'>Edit You Shop Profile</Accordion.Header>
-              <Accordion.Body className='editShop-acc'>
-||||||| merged common ancestors
-  return (
-<Accordion defaultActiveKey="0" >
-  <Container>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header className='editShop-accHead'>Edit You Shop Profile</Accordion.Header>
-        <Accordion.Body className='editShop-acc'>
-=======
-
-  return (
-<Accordion defaultActiveKey="0" >
-  <Container>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header className='editShop-accHead'>Edit You Shop Profile</Accordion.Header>
-        <Accordion.Body className='editShop-acc'>
->>>>>>> main
-
-                <Row>
-                  <ShopEditForm
-                    shop={props.shop}
-                    editShopCb={props.editShopCb}
-                  />
-                </Row>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>Shop Products</Accordion.Header>
-              <Accordion.Body>
-                <Row>
-                  <Col>
-                    <SellerForm addProductCb={addProduct}
-                      showProducts={props.showAllProducts} /> //Getting from the App
-                  </Col>
-                  <Col>
-                    <SellerList productsData={productsData}
-                      deleteProductCb={(shop_id, product_id) => deleteProduct(shop_id, product_id)}
-                      editProductCb={(id, formData) => editProduct(id, formData)}//id sent to the sellerDash (parent of sellerlist)
-                    /></Col>
-      
-                </Row>
-    
-       
-              </Accordion.Body>
-            </Accordion.Item>
-          </Container>
-        </Accordion>
-    
-      );
+    } catch (err) {
+      console.log(`Server error: ${err.message}`);
     }
-  
+  }
 
-
+  return (
+    <Accordion defaultActiveKey="0">
+      <Container>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header className="editShop-accHead">
+            Edit You Shop Profile
+          </Accordion.Header>
+          <Accordion.Body className="editShop-acc">
+            <Row>
+              <ShopEditForm shop={props.shop} editShopCb={props.editShopCb} />
+            </Row>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Shop Products</Accordion.Header>
+          <Accordion.Body>
+            <Row>
+              <Col>
+                <SellerForm
+                  addProductCb={addProduct}
+                  showProducts={props.showAllProducts}
+                />{" "}
+                //Getting from the App
+              </Col>
+              <Col>
+                <SellerList
+                  productsData={productsData}
+                  deleteProductCb={(shop_id, product_id) =>
+                    deleteProduct(shop_id, product_id)
+                  }
+                  editProductCb={(id, formData) => editProduct(id, formData)} //id sent to the sellerDash (parent of sellerlist)
+                />
+              </Col>
+            </Row>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Container>
+    </Accordion>
+  );
+}
 
 export default SellerDash;
