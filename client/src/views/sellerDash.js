901050910101fe751b4e30a5ac9eef23be8e4f8a
Megan Walsh <megan.katie.walsh@gmail.com>
2022-11-24 10:04:03 +0100

    thurssss

--- client/src/views/sellerDash.js
+++ client/src/views/sellerDash.js
@@ -36,115 +36,116 @@ function SellerDash(props) {
     let options = {
       method: "POST",
       //headers: { 'Content-Type': 'application/json' }, //remove?
-      body:  formData
+      body: formData
     };
 
-  try {
-    let response = await fetch(`/products/${Local.getShopId()}`, options); 
-    if (response.ok) {
-    let result = await response.json();
-    setProductsData(result);
-    getProducts();
-    props.showAllProducts(); // fetches all the products without reload on the online store
-     //note to ask about the reload
-    } else {
-      console.log(`Server error: ${response.status} ${response.statusText}`);
-    }
-    // add token to headers if it exists in localStorage
-    let token = Local.getToken();
-    if(token) {
-        options.headers['authorization'] = `Bearer ${token}`;
-    }
-    
     try {
-      let response = await fetch(`/products/${Local.getShopId()}`, options); 
-      if (response.ok) {
-      let result = await response.json();
-      setProductsData(result);
-      getProducts(); //note to ask about the reload
-      } else {
-        console.log(`Server error: ${response.status} ${response.statusText}`);
-      }
-    } catch (err) {
-        console.log(`Network error: ${err.message}`);
-      }
-  }
-
-  async function deleteProduct(shop_id, product_id) {
-    let options = {
-      method: "DELETE",
-    };
-  try {
-    let response = await fetch(`/products/${shop_id}/${product_id}`, options); 
-
-    if (response.ok) {
-      let result = await response.json();
-      setProductsData(result);
-      getProducts()
-      props.showAllProducts(); //split second loads all products
-    } else {
-      console.log(`Server error: ${response.status} ${response.statusText}`);
-    }
-  }
-
-    async function editProduct(shop_id, product_id, formData) {
-      let options = {
-      method: 'PUT',
-      // headers: { 'Content-Type': 'application/json' },
-      body: formData,
-  };
-
-  try {
-      let response = await fetch(`/products/${shop_id}/${product_id}`, options);
+      let response = await fetch(`/products/${Local.getShopId()}`, options);
       if (response.ok) {
         let result = await response.json();
         setProductsData(result);
+        getProducts();
+        props.showAllProducts(); // fetches all the products without reload on the online store
+        //note to ask about the reload
       } else {
         console.log(`Server error: ${response.status} ${response.statusText}`);
       }
-    } catch (err) {
-      console.log(`Server error: ${err.message}`);
+      // add token to headers if it exists in localStorage
+      let token = Local.getToken();
+      if (token) {
+        options.headers['authorization'] = `Bearer ${token}`;
+      }
+    
+      try {
+        let response = await fetch(`/products/${Local.getShopId()}`, options);
+        if (response.ok) {
+          let result = await response.json();
+          setProductsData(result);
+          getProducts(); //note to ask about the reload
+        } else {
+          console.log(`Server error: ${response.status} ${response.statusText}`);
+        }
+      } catch (err) {
+        console.log(`Network error: ${err.message}`);
+      }
     }
-  }
 
-  return (
-<Accordion defaultActiveKey="0" >
-  <Container>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header className='editShop-accHead'>Edit You Shop Profile</Accordion.Header>
-        <Accordion.Body className='editShop-acc'>
+  async function deleteProduct(shop_id, product_id) {
+      let options = {
+        method: "DELETE",
+      };
+      try {
+        let response = await fetch(`/products/${shop_id}/${product_id}`, options);
 
-      <Row>
-        <ShopEditForm 
-          shop = {props.shop}
-          editShopCb={props.editShopCb}
-        />
-      </Row>
-      </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Shop Products</Accordion.Header>
-        <Accordion.Body>
-      <Row>
-      <Col>
-      <SellerForm addProductCb={addProduct}
-                  showProducts={props.showAllProducts} /> //Getting from the App
-      </Col>
-      <Col>
-      <SellerList productsData={productsData}
-                  deleteProductCb={(shop_id, product_id) => deleteProduct(shop_id, product_id)}
-                  editProductCb={(id, formData) =>editProduct(id, formData)}//id sent to the sellerDash (parent of sellerlist)
-      /></Col> 
+        if (response.ok) {
+          let result = await response.json();
+          setProductsData(result);
+          getProducts()
+          props.showAllProducts(); //split second loads all products
+        } else {
+          console.log(`Server error: ${response.status} ${response.statusText}`);
+        }
+      }
+
+    async function editProduct(shop_id, product_id, formData) {
+        let options = {
+          method: 'PUT',
+          // headers: { 'Content-Type': 'application/json' },
+          body: formData,
+        };
+
+        try {
+          let response = await fetch(`/products/${shop_id}/${product_id}`, options);
+          if (response.ok) {
+            let result = await response.json();
+            setProductsData(result);
+          } else {
+            console.log(`Server error: ${response.status} ${response.statusText}`);
+          }
+        } catch (err) {
+          console.log(`Server error: ${err.message}`);
+        }
+      }
+
+      return (
+        <Accordion defaultActiveKey="0" >
+          <Container>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header className='editShop-accHead'>Edit You Shop Profile</Accordion.Header>
+              <Accordion.Body className='editShop-acc'>
+
+                <Row>
+                  <ShopEditForm
+                    shop={props.shop}
+                    editShopCb={props.editShopCb}
+                  />
+                </Row>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Shop Products</Accordion.Header>
+              <Accordion.Body>
+                <Row>
+                  <Col>
+                    <SellerForm addProductCb={addProduct}
+                      showProducts={props.showAllProducts} /> //Getting from the App
+                  </Col>
+                  <Col>
+                    <SellerList productsData={productsData}
+                      deleteProductCb={(shop_id, product_id) => deleteProduct(shop_id, product_id)}
+                      editProductCb={(id, formData) => editProduct(id, formData)}//id sent to the sellerDash (parent of sellerlist)
+                    /></Col>
       
-      </Row>
+                </Row>
     
        
-      </Accordion.Body>
-      </Accordion.Item>
-    </Container>
-    </Accordion>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Container>
+        </Accordion>
     
-  );
-}
+      );
+    }
+  
 
 export default SellerDash;
